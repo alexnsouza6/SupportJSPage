918a9a92aee9223a59ffe10d8a24e93b96c44fc6
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-10 16:23:47 -0200

    feat: Adding input and textarea

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
-  state = { comment: '' };  
+  state = { 
+    title: '',
+    comment: ''
+  };  
+
+  handleTitle = (event) => {
+    this.setState({ title: event.target.value });
+  }
+
+  handleComment = (event) => {
+    this.setState({ comment: event.target.value });
+  }
+
+
   render() { 
     return ( 
-      <div class="container">
+      <div>
         <h1>Welcome to Single Page Project!</h1>
         <h2>Please, Enter a Subject with some description.</h2>
+        <form>
+          <label>Subject: </label>
+          <input
+            onChange={ this.handleTitle }
+            value={ this.state.title }
+          />
+          <label> Description: </label>
+          <textarea
+            onChange={ this.handleComment }
+            value={ this.state.comment }
+          />
+        </form>
       </div>
     );
   }
