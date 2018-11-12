0d876cb5b5f81ae53ca1311ce3b6f4e3aad40777
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-12 17:02:29 -0200

    refactor: Fragment instead of div

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-notifications/lib/notifications.css';
-import React from 'react';
+import React, { Fragment } from 'react';
 import CommentBox from './CommentBox';
 import CommentList from './CommentList';
 
 export default () => (
-  <div>
+  <Fragment>
     <CommentBox />
     <CommentList />
-  </div>
+  </Fragment>
 );
