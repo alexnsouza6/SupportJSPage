5eb454e0a1cf4ae238699468bf3544488f0608cd
alexnsouza6 <alexnsouza6@hotmail.com>
2018-11-11 23:41:55 -0200

    feat: root component for wrapp store in tests

---
+++ src/Root.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
+
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+
+export default (props) => {
+  return(
+    <Provider store={store}>
+      { props.children }
+    </Provider>
+  );
+}
+
